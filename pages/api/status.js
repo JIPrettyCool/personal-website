f5e87208b86d040ecae8095f94bceea89077a17d
JI <49317513+JIPrettyCool@users.noreply.github.com>
2023-05-15 19:53:54 +0300

    damn

--- pages/api/status.js
+++
@@ -1,8 +0,0 @@
-let link = "https://api.lanyard.rest/v1/users/455384255774720011"
-export default async (req, res) => {
-    let istek = await fetch(link, {
-        
-    })
-    let veri = await istek.json()
-    return res.json(veri)
-}
