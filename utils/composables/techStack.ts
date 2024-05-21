244dbfaa66a8d81bce6a52c063901de39b398939
Meric <tierbestdev@gmail.com>
2024-05-21 18:00:29 +0300

    31

--- utils/composables/techStack.ts
+++ utils/composables/techStack.ts
@@ -6,13 +6,13 @@ export const techStack = [
     url: "https://github.com/JIPrettyCool/text-based-football",
   },
   {
-    icon: "vscode-icons:file-python",
+    icon: "vscode-icons:file-type-python",
     color: "#3974a5",
     name: "🔥 CS:GO Hack (archived)",
     url: "https://github.com/JIPrettyCool/hammer-csgo-hack",
   },
   {
-    icon: "vscode-icons:file-type-ts",
+    icon: "vscode-icons:file-type-typescript-official",
     color: "#27609e",
     name: "❌Never Rick Roll",
     url: "https://github.com/JIPrettyCool/neverrickroll",
